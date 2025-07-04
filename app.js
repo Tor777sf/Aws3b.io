b22243ebe633bfad35d16bd87f531b11178c986f
Tor777sf <126281406+Tor777sf@users.noreply.github.com>
2025-07-04 11:41:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const awsconfig = {
     oauth: {
       domain: 'us-east-2u6vqt05nv.auth.us-east-2.amazoncognito.com',
       scope: ['openid', 'email'],
-      redirectSignIn: 'https://to777sf.github.io/Aws3b/callback.html',
-      redirectSignOut: 'https://to777sf.github.io/Aws3b/index.html',
+      redirectSignIn: 'https://tor777sf.github.io/Aws3b/callback.html',
+      redirectSignOut: 'https://tor777sf.github.io/Aws3b/index.html',
       responseType: 'code'
     }
   },
